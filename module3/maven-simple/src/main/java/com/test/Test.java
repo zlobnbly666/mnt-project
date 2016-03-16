d48637a84553120528a5615141175308882a507c
zlobnbly666 <lapshyn87@yandex.ru>
2016-03-16 13:55:44 +0300

    Update Test.java

--- module3/maven-simple/src/main/java/com/test/Test.java
+++ module3/maven-simple/src/main/java/com/test/Test.java
@@ -1,9 +1,9 @@
 package com.test;
-
+//saasasasasasaasasasasasas
 class Test {
-
+//asasasasasasasasasasasasas
   public static void main(String[] args) {
     System.out.println("Hellow MTN");
   }
-
+//asasasasasasasasasasasasas
 }
