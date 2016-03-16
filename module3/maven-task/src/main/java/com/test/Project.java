932e3436e4c159508afec547ef56edd0fb216f73
zlobnbly666 <lapshyn87@yandex.ru>
2016-03-16 13:50:06 +0300

    Update Project.java

--- module3/maven-task/src/main/java/com/test/Project.java
+++ module3/maven-task/src/main/java/com/test/Project.java
@@ -1,16 +1,16 @@
 package com.test;
-
+#absbsbsbsbsbsbsbsbsbsbsbsbbs
 //dependency on Apache commons-collections
 import org.apache.commons.collections.Predicate;
-
+#absbsbsbsbsbsbsbsbsbsbsbsbbs
 class Project {
-
+#absbsbsbsbsbsbsbsbsbsbsbsbbs
   public static void test() {
     System.out.println("test");
   }
-  
+  #absbsbsbsbsbsbsbsbsbsbsbsbbs
   public static void main(String[] args) {
     System.out.println("Hellow MTN");
   }
-
+#absbsbsbsbsbsbsbsbsbsbsbsbbs
 }
